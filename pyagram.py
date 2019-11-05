77358298a7db24a05c43595104529ff726a88405
sequoia-tree <sequoia@berkeley.edu>
2019-11-05 09:17:28 -0800

    Some documentation, mostly just want to see if my RSA key works

--- pyagram.py
+++ pyagram.py
@@ -6,9 +6,14 @@ import trace
 
 class Pyagram:
     """
-    <summary> # Basically a Pyagram object is a list of snapshots.
+    A diagram describing the step-by-step execution of a snippet of Python code.
 
-    :param code:
+    After initialization, :self.snapshots: is a list of serializable snapshots. Each such snapshot
+    encodes the state of the pyagram at a particular step during the execution of the input code.
+
+    :param code: The snippet of Python code for which to create the pyagram.
+    :param debug: Whether or not to enable debugging features such as on-the-fly printing of the
+      pyagram during its construction.
     """
 
     def __init__(self, code, *, debug):
@@ -21,7 +26,7 @@ class Pyagram:
 
     def serialize(self):
         """
-        <summary>
+        Produce a serialization  ???  JSON string
 
         :return:
         """
