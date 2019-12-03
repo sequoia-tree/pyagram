1602b510c81d7d330422130d8768a64783feb1ea
sequoia-tree <sequoia@berkeley.edu>
2019-12-03 16:24:13 -0500

    Clean up SCSS files.

--- src/scripts/draw.js
+++ src/scripts/draw.js
@@ -3,5 +3,8 @@ export function drawPyagram(renderedComponents) {
     var printOutputPane = document.getElementById('print-output');
     // TODO: Draw pyagram.
     // TODO: Draw print output.
+    // var programState = renderedComponents.program_state;
+    // var memoryState = renderedComponents.memory_state;
+    // var printOutput = renderedComponents.print_output;
     printOutputPane.innerHTML = 'test: line one\ntest: line two';
 };
