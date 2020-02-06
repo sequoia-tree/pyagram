550cbcd7cc4e2268b8fc7386a97cc0f0aec56df8
sequoia-tree <sequoia@berkeley.edu>
2020-02-05 22:24:34 -0800

    Update todos.

--- src/scripts/pyagram.js
+++ src/scripts/pyagram.js
@@ -25,6 +25,7 @@ export function drawPyagram(slider, pyagram) {
 export function drawSnapshot(snapshotIndex, pyagram, printOutput) {
     pyagram.innerHTML = snapshots[snapshotIndex].state;
     // TODO: Use the other attributes of snapshots[snapshotIndex].
+    // TODO: Instead of having layout.scss, just use jQuery to get the height of the editor + scrollbar, and set that element to have min-height: X + "px".
 }
 
 // TODO: Once everything works like it did before winter break, delete the directory pyagram/src/REMOVE-THIS-OLD-STUFF.
