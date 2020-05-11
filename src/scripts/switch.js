773af98857233a5273865014ad55e1366e644d37
sequoia-tree <sequoia@berkeley.edu>
2020-05-11 12:07:09 -0400

    Refactor SCSS files.

--- src/scripts/switch.js
+++ src/scripts/switch.js
@@ -1,15 +1,11 @@
 const ACTIVE_CLASS_NAME = 'active';
-const INACTIVE_CLASS_NAME = 'inactive';
 
 export function select(parent, childId) {
     var child;
     for (child of parent.children) {
         child.classList.remove(ACTIVE_CLASS_NAME);
-        child.classList.remove(INACTIVE_CLASS_NAME);
         if (child.id === childId) {
             child.classList.add(ACTIVE_CLASS_NAME);
-        } else {
-            child.classList.add(INACTIVE_CLASS_NAME);
         }
     }
 }
